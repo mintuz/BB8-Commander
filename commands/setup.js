95d2ad18b72c3852fa9091bc9c89843fe1ac38ab
Shaun Bent <shaunbent@me.com>
2016-01-18 21:52:44 +0000

    Better connect message

--- commands/setup.js
+++ commands/setup.js
@@ -2,42 +2,45 @@ var noble = require('noble'),
     _ = require('lodash');
 
 module.exports = function() {
-  
-  console.log('Beginning Setup');
 
-  noble.on('stateChange', function(state) {
-    
-    if (state === 'poweredOn') {
-      noble.startScanning();
-    } else {
-      noble.stopScanning();
-    }
+    console.log('Beginning Setup');
 
-  });
+    noble.on('stateChange', function(state) {
 
-  noble.on('discover', function(peripheral){
+        if (state === 'poweredOn') {
+            noble.startScanning();
+        } else {
+            noble.stopScanning();
+        }
 
-    if(_.includes(peripheral.advertisement.localName, 'BB-')) {
+    });
 
-      var deviceUUID = peripheral.uuid;
+    noble.on('discover', function(peripheral){
 
-      console.log('BB8 UUID - "' + deviceUUID + '"');
-      console.log('Writing to config file');
+        if(_.includes(peripheral.advertisement.localName, 'BB-')) {
 
-      var config = require('home-config').load('.bb8config', {
-        BB8_UUID: deviceUUID
-      });
+            var deviceUUID = peripheral.uuid,
+            localName = peripheral.advertisement.localName;
 
-      config.save();
+            console.log('BB8 UUID - "' + deviceUUID + '"');
+            console.log('Writing to config file');
+            console.log("Local Name - " + localName);
 
-      console.log('Saved config file, you can now ctrl+c this task');
+            var config = require('home-config').load('.bb8config', {
+                BB8_UUID: deviceUUID,
+                BB8_LOCAL_NAME: localName
+            });
 
-    } else {
-      console.log("This isn't the droid you are looking for");
-      console.log("UUID - " + peripheral.uuid);
-      console.log("Local Name - " + peripheral.advertisement.localName);
-    }
+            config.save();
 
-  });
+            console.log('Saved config file, you can now ctrl+c this task');
+
+        } else {
+            console.log("This isn't the droid you are looking for");
+            console.log("UUID - " + peripheral.uuid);
+            console.log("Local Name - " + peripheral.advertisement.localName);
+        }
+
+    });
 
 };
