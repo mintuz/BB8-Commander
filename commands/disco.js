b95b58297490a34f7b25d75f339b084abe103351
Paul Hammond <paul.hammond@bbc.co.uk>
2016-01-19 23:17:27 +0000

    turning the disco command into just a command

--- commands/disco.js
+++ commands/disco.js
@@ -1,23 +1,8 @@
-var bb8 = require('../libs/bb8-instance')();
-var config = require('../libs/bb8-instance').config;
+module.exports = function (bb8) {
+    console.log('Let\'s Party!!');
 
-module.exports = function() {
-    if(bb8) {
-        bb8.connect(function() {
-
-            console.log('Connected to ' + config.BB8_LOCAL_NAME);
-            console.log('Let\'s Party!!');
-            
-            bb8.randomColor();
-
-            setInterval(function() {
-
-                bb8.randomColor();
-
-            }, 1000);
-
-        });
-
-    }
-
+    bb8.randomColor();
+    setInterval(function () {
+        bb8.randomColor();
+    }, 1000);
 };
