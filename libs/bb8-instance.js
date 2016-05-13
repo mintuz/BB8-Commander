4e93f4787d7122553d1aa13dd0817b7131d26da9
Sudheer Reddy Puthana <sudheer@sudheer.co.in>
2016-05-13 18:36:27 -0400

    Drive Mode #2
    Enable input from keyboard to 'drive' the BB-8 in the desired direction.
    Takes input in wsad style.
    Has callibration now.
    See console log messages for more inputs.
    Add the use of keypress module to control the keyboard inputs.
    Add a semicolon in libs/bb8-instance.js.

--- libs/bb8-instance.js
+++ libs/bb8-instance.js
@@ -2,12 +2,12 @@ var sphero = require("sphero"),
     config = require('home-config').load('.bb8config');
 
 module.exports = function() {
-  
+
   if(typeof(config.BB8_UUID) !== 'undefined') {
     return sphero(config.BB8_UUID);
   }
 
   return false;
-}
+};
 
 module.exports.config = config;
