0c2b8dac8ff61b3791753f26f7f090045b5ef421
Victor Hugo Brito Fernandes <victorhbfernandes@gmail.com>
2018-04-09 22:20:46 -0300

    fixing counter name and adding lodash to color sampler

--- commands/christmaspatrol.js
+++ commands/christmaspatrol.js
@@ -1,22 +1,15 @@
 module.exports = function (bb8,options) {
- var radius = options.radius; //mm
- var distance = 2*Math.PI*radius;
- var colors = ["red","green"];
- var i = 0;
- return setInterval(function() {
+    var radius = options.radius; //mm
+    var distance = 2*Math.PI*radius;
+    var colors = ["red","green"];
+    var angle_counter = 0;
     var direction = 30;
-    bb8.roll(Math.floor(distance/12),direction*i);
-    var chosen = (function(){
-      if (Math.random() > 0.5) {
-        return 1;
-       } else {
-        return 0;
-       }
-    })();
-    bb8.color(colors[chosen]);
-    i++;
-    if (i>11) {
-        i = 0;
-    }
- }, 500);
+    return setInterval(function() {
+        bb8.roll(Math.floor(distance/12),direction*angle_counter);
+        bb8.color(_.sample(colors));
+        angle_counter++;
+        if (angle_counter>11) {
+            angle_counter = 0;
+        }
+    }, 500);
 };
